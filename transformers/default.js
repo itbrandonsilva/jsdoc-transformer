c79a772785fb4f777f183979e540635f4fde85b2
Brandon Silva <itbrandonsilva@gmail.com>
2015-11-20 12:15:35 -0500

    Initial commit.

---
+++ transformers/default.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var _ = require('lodash');
+
+var style = {};
+
+style = {
+    class: {
+        template: '## {{name}} {{paramsSummary}}  \n{{description}}\n\n{{params}}',
+        params: {
+            template: '{{name}}: {{description}}  \n'
+        },
+        paramsSummary: {
+            template: function (comment) {
+                return '(' + comment.join(', ') + ')';
+            }
+        }
+    }
+};
+
+style.function = style.class;
+
+module.exports = {
+    style: style,
+    extend: function (comment) {
+        if (['function', 'class'].indexOf(comment.kind) == -1) return;
+        comment.paramsSummary = comment.params.map(function (param) {
+            return param.name + ':' + param.type.names[0];
+        });
+    }
+}
